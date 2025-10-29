f563b5905d7222ee76b204422c2f958e7464bb4e
Seif Seoud <seif.seoud@intalio.com>
2025-10-29 17:07:25 +0300

    at 6

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,4 +1,6 @@
 ﻿using Carter;
+using Mapster;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
 
@@ -8,6 +10,17 @@ public class CreateProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        throw new NotImplementedException();
+        app.MapPost("/products",
+            async (CreateProductRequest request, ISender sender) =>
+            {
+                var command =request.Adapt<CreateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateProductResponse>();
+                return Results.Created($"/products/{response.Id}", response);
+            }).WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+  config.RegisterServicesFromAssemblies(typeof(Program).Assembly);   
+});
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions(); 
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
 //app.MapGet("/", () => "Hello World!");
+app.MapCarter();
 app.Run();
+ 
